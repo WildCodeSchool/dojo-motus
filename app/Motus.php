ee3ca48877e4ebce47a21b675c49dd66bd6b61f3
guillaumebdx <guillaumeharari@hotmail.com>
2022-01-24 20:04:41 +0100

    repo ready to run

---
+++ app/Motus.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+class Motus
+{
+    public function check(string $attempt, string $expected): string
+    {
+        return '';
+    }
+}
---
+++ app/MotusTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use PHPUnit\Framework\TestCase;
+
+class MotusTest extends TestCase
+{
+    public function testCheckWord()
+    {
+        $motus = new Motus();
+        $this->assertEquals('Nombre de lettres incorrect', $motus->check('MOT', 'MOTUS'));
+//        $this->assertEquals('🟩', $motus->check('A', 'A'));
+//        $this->assertEquals('🟩🟩', $motus->check('AS', 'AS'));
+//        $this->assertEquals('🟦', $motus->check('B', 'A'));
+//        $this->assertEquals('🟦🟦', $motus->check('BI', 'AS'));
+//        $this->assertEquals('🟦🟩🟦', $motus->check('MER', 'TEK'));
+//        $this->assertEquals('🟩🟩🟩🟩🟩', $motus->check('MOTUS', 'MOTUS'));
+//        $this->assertEquals('🟦🟦🟦🟦🟦', $motus->check('CARRE', 'MOTUS'));
+//        $this->assertEquals('🟡🟡', $motus->check('NU', 'UN'));
+//        $this->assertEquals('🟡🟩🟩🟩🟦', $motus->check('SOTUA', 'MOTUS'));
+//        //these following tests should be bonus : handle juste ONE yellow by occurence
+//        $this->assertEquals('🟡🟦🟦', $motus->check('TTA', 'MOT'));
+//        $this->assertEquals('🟦🟦🟩🟦🟦', $motus->check('TTTTT', 'MOTUS'));
+    }
+}
