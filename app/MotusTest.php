c686c13983de00de3833fa6d2bf5448a8949fbcc
guillaumebdx <guillaumeharari@hotmail.com>
2022-01-25 00:39:23 +0100

    dojo ready

--- app/MotusTest.php
+++ app/MotusTest.php
@@ -10,20 +10,21 @@ class MotusTest extends TestCase
     {
         $motus = new Motus();
         $this->assertEquals('Nombre de lettres incorrect', $motus->check('MOT', 'MOTUS'));
-        $this->assertEquals('🟩', $motus->check('A', 'A'));
-        $this->assertEquals('🟩🟩', $motus->check('AS', 'AS'));
-        $this->assertEquals('🟦', $motus->check('B', 'A'));
-        $this->assertEquals('🟦🟦', $motus->check('BI', 'AS'));
-        $this->assertEquals('🟦🟩🟦', $motus->check('MER', 'TEK'));
-        $this->assertEquals('🟩🟩🟩🟩🟩', $motus->check('MOTUS', 'MOTUS'));
-        $this->assertEquals('🟦🟦🟦🟦🟦', $motus->check('CARRE', 'MOTUS'));
-        $this->assertEquals('🟡🟡', $motus->check('NU', 'UN'));
-        $this->assertEquals('🟡🟩🟩🟩🟦', $motus->check('SOTUA', 'MOTUS'));
+//        $this->assertEquals('🟩', $motus->check('A', 'A'));
+//        $this->assertEquals('🟩🟩', $motus->check('AS', 'AS'));
+//        $this->assertEquals('🟦', $motus->check('B', 'A'));
+//        $this->assertEquals('🟦🟦', $motus->check('BI', 'AS'));
+//        $this->assertEquals('🟦🟩🟦', $motus->check('MER', 'TEK'));
+//        $this->assertEquals('🟩🟩🟩🟩🟩', $motus->check('MOTUS', 'MOTUS'));
+//        $this->assertEquals('🟦🟦🟦🟦🟦', $motus->check('CARRE', 'MOTUS'));
+//        $this->assertEquals('🟡🟡', $motus->check('NU', 'UN'));
+//        $this->assertEquals('🟡🟩🟩🟩🟦', $motus->check('SOTUA', 'MOTUS'));
 //        //these following tests should be bonus : handle juste ONE yellow by occurence
-        $this->assertEquals('🟡🟦🟦', $motus->check('TTA', 'MOT'));
-        $this->assertEquals('🟦🟦🟩🟦🟦', $motus->check('TTTTT', 'MOTUS'));
-        $this->assertEquals('🟡🟡🟡🟦🟩🟡🟩🟦', $motus->check('CHISINAU', 'MICHIGAN'));
-        $this->assertEquals('🟩🟡🟦🟦🟩🟦🟡🟦', $motus->check('MALAISIE', 'MICHIGAN'));
-        $this->assertEquals('🟩🟡🟦🟦🟩🟦🟡🟦', $motus->check('MASSILIA', 'MICHIGAN'));
+//        $this->assertEquals('🟡🟦🟦', $motus->check('TTA', 'MOT'));
+//        $this->assertEquals('🟦🟦🟩🟦🟦', $motus->check('TTTTT', 'MOTUS'));
+          //these following bonus are very difficult to implement. DO NOT DO IT AT HOME
+//        $this->assertEquals('🟡🟡🟡🟦🟩🟡🟩🟦', $motus->check('CHISINAU', 'MICHIGAN'));
+//        $this->assertEquals('🟩🟡🟦🟦🟩🟦🟡🟦', $motus->check('MALAISIE', 'MICHIGAN'));
+//        $this->assertEquals('🟩🟡🟦🟦🟩🟦🟡🟦', $motus->check('MASSILIA', 'MICHIGAN'));
     }
 }
